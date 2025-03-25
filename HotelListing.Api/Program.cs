9bb791e3446831da191cf25a1dff31c5b41535ac
twilliams <trevoir.williams@gmail.com>
2025-03-25 15:13:22 -0500

    Added space to Porgram.cs

--- HotelListing.Api/Program.cs
+++ HotelListing.Api/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
